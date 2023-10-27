6fe13827581922eac19ae6e1bb1709653e8d3f52
Maks Charyev <helloimaksim@gmail.com>
2023-10-27 13:52:18 +0300

    Adding first code
     Добавление первоначальных папок, конфига, для работы со шлюзом

---
+++ FitTeamAPI/Context/DatabaseContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace FitTeamAPI.Context
+{
+    public class DatabaseContext: DbContext
+    {
+        public DatabaseContext() { }
+
+    }
+}
---
+++ FitTeamAPI/Models/Models.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace FitTeamAPI.Models
+{
+    public class Worker
+    {
+        [Key] 
+        public int UUID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+    public class Role
+    {
+        [Key]
+        public int RoleID { get; set; }
+        public string RoleName { get; set; }
+        public string RoleDescription { get; set; }
+        
+    }
+    public class Permission
+    {
+        [Key]
+        public int PermissionID { get; set; }
+        public string PermissionName { get; set; }
+        public string PermissionDescription { get; set; }
+    }
+
+}
